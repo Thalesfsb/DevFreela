5b44f458449287129129119380adf76c5b4d17f6
Thales Silveira <thales.silveira@sinqia.com.br>
2024-09-12 11:09:33 -0300

    SHA256
    SHA256

--- DevFreela.Application/Commands/Users/InsertUser/InsertUserHandler.cs
+++ DevFreela.Application/Commands/Users/InsertUser/InsertUserHandler.cs
@@ -1,5 +1,6 @@
 ﻿using DevFreela.Application.ViewModel;
 using DevFreela.Core.Repositories;
+using DevFreela.Core.Services;
 using DevFreela.Infrastructure.Persistence;
 using MediatR;
 
@@ -8,13 +9,16 @@ namespace DevFreela.Application.Commands.Users.InsertUser
     public class InsertUserHandler : IRequestHandler<InsertUserCommand, ResultViewModel<int>>
     {
         private readonly IUserRepository _repository;
-
-        public InsertUserHandler(IUserRepository repository)
+        private readonly IAuthService _authService;
+        public InsertUserHandler(IUserRepository repository, IAuthService authService)
         {
             _repository = repository;
+            _authService = authService;
         }
         public async Task<ResultViewModel<int>> Handle(InsertUserCommand request, CancellationToken cancellationToken)
         {
+            request.Password = _authService.ComputeSha256Hash(request.Password);
+
             var result = request.ToEntity();
 
             if (await _repository.Exists(request.Id))
--- DevFreela.Infrastructure/Auth/AuthService.cs
+++ DevFreela.Infrastructure/Auth/AuthService.cs
@@ -19,13 +19,33 @@ namespace DevFreela.Infrastructure.Auth
         {
             _configuration = configuration;
         }
+
+        public string ComputeSha256Hash(string password)
+        {
+            //Inicializando o método do sha256 Create
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                //ComputeHash - retorna byte array
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+                //Converte byte array para string - Concatenação de string
+                StringBuilder builder = new StringBuilder();
+
+                // 2x faz com que seja convertido em representação hexadecimal
+                for (int i = 0; i < bytes.Length; i++)
+                    builder.Append(bytes[i].ToString("x2"));
+
+                return builder.ToString();
+            }
+        }
+
         public string GenerateJwtToker(string email, string role)
         {
             var issuer = _configuration["Jwt:Issuer"];
             var audience = _configuration["Jwt:Audience"];
             var key = _configuration["Jwt:Key"];
 
-            var securitykey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key??""));
+            var securitykey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key ?? ""));
             var credentials = new SigningCredentials(securitykey, SecurityAlgorithms.HmacSha256);
 
             var claims = new List<Claim>
@@ -34,10 +54,10 @@ namespace DevFreela.Infrastructure.Auth
                 new Claim(ClaimTypes.Role, role)
             };
 
-            var token = new JwtSecurityToken(issuer: issuer, 
-                                             audience: audience, 
+            var token = new JwtSecurityToken(issuer: issuer,
+                                             audience: audience,
                                              expires: DateTime.Now.AddMinutes(2),
-                                             signingCredentials : credentials,
+                                             signingCredentials: credentials,
                                              claims: claims);
 
             var tokenHandler = new JwtSecurityTokenHandler();
