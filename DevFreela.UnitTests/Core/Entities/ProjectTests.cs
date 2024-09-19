9d175b9b1465b36db85af50be419d15886300c42
Thales Silveira <thales.silveira@sinqia.com.br>
2024-09-19 12:09:58 -0300

    xUnit
    xUnit

---
+++ DevFreela.UnitTests/Core/Entities/ProjectTests.cs
@@ -0,0 +1,38 @@
+﻿using DevFreela.Core.Entities;
+using DevFreela.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DevFreela.UnitTests.Core.Entities
+{
+    public class ProjectTests
+    {
+        [Fact]
+        public void TestIfProjectWorks()
+        {
+            var project = new Project("Nome de teste", "Descrição de teste", 1, 2, 100000);
+
+            // Validando a inicializacao do objeto
+            Assert.Equal(ProjectStatusEnum.Created, project.Status);
+            Assert.Null(project.StartedAt);
+
+            Assert.NotNull(project.Title);
+            Assert.NotEmpty(project.Title);
+
+            Assert.NotNull(project.Description);
+            Assert.NotEmpty(project.Description);
+
+            project.Start();
+
+            //  Validando se o sart está ok
+            Assert.Equal(ProjectStatusEnum.InProgress, project.Status);
+            Assert.NotNull(project.StartedAt);
+
+            Assert.NotEmpty(project.Title);
+        }
+    }
+}
